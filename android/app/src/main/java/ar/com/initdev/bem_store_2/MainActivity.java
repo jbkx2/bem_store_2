5102d8ed6fa834a1519b4e2c5ee38bc2d17ca1f0
jbkx2 <braykal@hotmail.com>
2019-10-19 13:37:53 -0300

    Iniciando el proyecto de flutter

---
+++ android/app/src/main/java/ar/com/initdev/bem_store_2/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.com.initdev.bem_store_2;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:bem_store/Choice_Sport';
+
+void main() {
+  //Para correr widget de la vista
+  //La clase myapp
+  runApp(MyApp());
+}
+
+
